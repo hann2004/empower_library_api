4c19bfec51b262394e4d87fa25a3c37254c137bb
hann2004 <hananniya1996@gmail.com>
2025-10-12 18:16:19 +0300

    editing the comments

--- main.py
+++ main.py
@@ -6,27 +6,26 @@ import models
 import schemas
 from  database import engine, get_db
 from typing import List
-# Create database tables
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# ✅ Create a new book
 @app.post("/books", response_model=schemas.BookCreate)
 def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
     return crud.create_book(db=db, book=book)
 
-# ✅ Get all books
+
 @app.get("/books", response_model=List[schemas.BookCreate])
 def get_books(db: Session = Depends(get_db)):
     return crud.get_books(db)
 
-# ✅ Get a single book
+
 @app.get("/books/{book_id}", response_model=schemas.BookCreate)
 def get_book(book_id: int, db: Session = Depends(get_db)):
     return crud.get_book_by_id(db, book_id)
 
-# ✅ Update a book
+
 @app.put("/books/{book_id}", response_model=schemas.BookCreate)
 def update_book(book_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)):
     return crud.update_book(db, book_id, book)
