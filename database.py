bfd184f0e10115a1e134fb089dab16041fe5cbea
hann2004 <hananniya1996@gmail.com>
2025-10-18 15:34:06 +0300

    making .env file for password

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
+import os
 from sqlalchemy.orm import sessionmaker, declarative_base
+# Load from .env file
+from dotenv import load_dotenv
+load_dotenv()  # This loads the .env file
 
-DATABASE_URL = "postgresql://nabi:nabi24@localhost:5432/library_db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
