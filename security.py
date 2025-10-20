5c244b74406f8ee0e4dbbb5d14ec794d53f910f8
hann2004 <hananniya1996@gmail.com>
2025-10-20 18:15:34 +0300

    updating security.py

--- security.py
+++ security.py
@@ -1,4 +1,14 @@
 import bcrypt
+import os
+from dotenv import load_dotenv
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+
+load_dotenv()
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30    
+REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 def get_hash_password(password: str) -> str:
     """Hash a password using bcrypt"""
@@ -16,3 +26,30 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
         return bcrypt.checkpw(plain_bytes, hashed_bytes)
     except Exception:
         return False
+    
+def create_access_token(data: dict)-> str:
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+def create_refresh_token(data: dict) -> str:
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+def verify_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
